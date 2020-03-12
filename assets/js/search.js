d372a2a356e0898676730ed7175b52ed4b6f9cf8
hamelsmu <hamelsmu@users.noreply.github.com>
2020-03-12 21:12:55 +0000

    deploy: c374c6d5fbb95925704975289c385c579d0e2dc3

--- assets/js/search.js
+++ assets/js/search.js
@@ -47,7 +47,7 @@ jtd.onReady = function(ready) {
 
 function initSearch() {
     var request = new XMLHttpRequest();
-    request.open('GET', '/covid19-dashboard/assets/js/search-data.json', true);
+    request.open('GET', '/assets/js/search-data.json', true);
   
     request.onload = function(){
       if (request.status >= 200 && request.status < 400) {
